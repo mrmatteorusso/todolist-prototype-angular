24cd2849c8ea34ba8ae6c784e04981c59adec08b
mrmatteorusso <mrmatteorusso@gmail.com>
2021-11-18 10:48:37 +0100

    todolist - basic  add/delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist-prototype-angular';
+  title = 'To do list ';
+  list: any[] = [];
+
+
+  addTask(item:string) {
+    console.log(item)
+    this.list.push({id: this.list.length, name: item})
+    console.log(this.list)
+  }
+
+  deleteTask(id:number){
+    this.list = this.list.filter(item => item.id != id)
+  }
+
+
 }
