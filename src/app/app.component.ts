1b68bf61a93eee706ddfca4a00a88deccbfe43db
mrmatteorusso <mrmatteorusso@gmail.com>
2021-11-24 10:00:16 +0100

    new input function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'To do list ';
   list: any[] = [];
-
+  newTask: any;
 
   addTask(item:string) {
+    item = this.newTask
     console.log(item)
     this.list.unshift({id: this.list.length, name: item})
     console.log(this.list)
+    this.newTask = '';
   }
 
   deleteTask(id:number){
@@ -22,3 +24,30 @@ export class AppComponent {
 
 
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'To do list ';
+//   list: any[] = [];
+//   newTask: string | undefined;
+
+//   addTask(item:string) {
+//     console.log(item)
+//     this.list.unshift({id: this.list.length, name: item})
+//     console.log(this.list)
+//     item = ''
+//   }
+
+//   deleteTask(id:number){
+//     this.list = this.list.filter(item => item.id != id)
+//   }
+
+
+// }
+
